d6ac2883142f7edad6b95178b00f7d3299d72893
StefanAngelovski <stefanangelovski2002@gmail.com>
2024-07-17 11:56:42 +0200

    Added new pages

--- Store/urls.py
+++ Store/urls.py
@@ -4,4 +4,8 @@ from Store import views
 app_name = 'Store'
 urlpatterns = [
     path('', views.display_index, name='index'),
+    path('about/', views.display_about, name='about'),
+    path('contact/', views.display_contact, name='contact'),
+    path('cart/', views.display_cart, name='cart'),
+    path('userProfile/', views.display_userProfile, name='userProfile'),
 ]
--- Store/views.py
+++ Store/views.py
@@ -6,3 +6,20 @@ from Store.models import Category
 # Create your views here.
 def display_index(request):
     return render(request, 'index.html', {'categories': Category.objects.all()})
+
+
+def display_about(request):
+    return render(request, 'about.html')
+
+
+def display_contact(request):
+    return render(request, 'contact.html')
+
+
+def display_cart(request):
+    return render(request, 'cart.html')
+
+
+def display_userProfile(request):
+    return render(request, 'userProfile.html')
+
